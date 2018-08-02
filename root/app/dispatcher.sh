ff4ba8a75e2d096d3ee1c3b4a6f9c73cd7bd9788
alex-phillips <ahp118@gmail.com>
2018-08-02 14:56:50 -0400

    config files renamed to higher numbers to prevent race conditions, config env variables now stored in arrays

--- root/app/dispatcher.sh
+++ root/app/dispatcher.sh
@@ -2,10 +2,12 @@
 
 . /config/.env
 
-TODAY=$(date +%Y-%m-%d)
-INDEX_PREFIX=${INDEX_PREFIX:-diskover-}
-INDEX_NAME=${INDEX_NAME:-$INDEX_PREFIX$TODAY}
-DISKOVER_OPTS=${DISKOVER_OPTS:-"-d /data -a"}
+# define array for input values
+declare -A DISKOVER_ARRAY
+DISKOVER_ARRAY[TODAY]=$(date +%Y-%m-%d)
+DISKOVER_ARRAY[INDEX_PREFIX]=${INDEX_PREFIX:-diskover-}
+DISKOVER_ARRAY[INDEX_NAME]=${INDEX_NAME:-$INDEX_PREFIX$TODAY}
+DISKOVER_ARRAY[DISKOVER_OPTS]=${DISKOVER_OPTS:-"-d /data -a"}
 
 DISKOVER_OPTS="$DISKOVER_OPTS -i $INDEX_NAME"
 
@@ -29,8 +31,8 @@ echo "killing dangling workers..."
 /bin/bash /app/diskover/diskover-bot-launcher.sh -k > /dev/null 2>&1
 sleep 3
 
-echo "starting workers with following options: $WORKER_OPTS"
-/bin/bash /app/diskover/diskover-bot-launcher.sh $WORKER_OPTS
+echo "starting workers with following options: ${DISKOVER_ARRAY[WORKER_OPTS]}"
+/bin/bash /app/diskover/diskover-bot-launcher.sh ${DISKOVER_ARRAY[WORKER_OPTS]}
 
-echo "starting crawler with following options: $DISKOVER_OPTS"
-/usr/bin/python3 ./diskover.py $DISKOVER_OPTS
+echo "starting crawler with following options: ${DISKOVER_ARRAY[DISKOVER_OPTS]}"
+/usr/bin/python3 ./diskover.py ${DISKOVER_ARRAY[DISKOVER_OPTS]}
