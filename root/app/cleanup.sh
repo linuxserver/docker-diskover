056ed8028b1a2364f40b0b4ed7730427e2a84b00
alex-phillips <ahp118@gmail.com>
2019-01-05 11:15:30 -0500

    bot / redis cleanup now in its own script so we can also call it when container is stopped / restarted

---
+++ root/app/cleanup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# killing existing workers before starting new ones
+echo "killing existing workers..."
+if [ -f "/tmp/diskover_bot_pids" ]; then
+    /bin/bash /app/diskover/diskover-bot-launcher.sh -k > /dev/null 2>&1
+    sleep 3
+fi
+
+# empty existing redis queue
+echo "emptying current redis queues..."
+rq empty -u redis://${DISKOVER_ARRAY[REDIS_HOST]}:${DISKOVER_ARRAY[REDIS_PORT]} diskover_crawl diskover diskover_calcdir failed
+sleep 3
+
+echo "killing dangling workers..."
+/bin/bash /app/diskover/diskover-bot-launcher.sh -k > /dev/null 2>&1
+sleep 3
