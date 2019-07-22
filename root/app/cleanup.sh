a522f59ac8cf1fd2ad541e31c36a4c297dbfc464
Chris Yocum <chryo12@yahoo.com>
2019-07-22 08:22:34 -0400

    Testing

--- root/app/cleanup.sh
+++ root/app/cleanup.sh
@@ -9,7 +9,7 @@ fi
 
 # empty existing redis queue
 echo "emptying current redis queues..."
-rq empty -u redis://${DISKOVER_ARRAY[REDIS_HOST]}:${DISKOVER_ARRAY[REDIS_PORT]} diskover_crawl diskover diskover_calcdir failed
+rq empty -u redis://"${DISKOVER_ARRAY[REDIS_HOST]}":"${DISKOVER_ARRAY[REDIS_PORT]}" diskover_crawl diskover diskover_calcdir failed
 sleep 3
 
 echo "killing dangling workers..."
