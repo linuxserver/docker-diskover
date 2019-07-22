6608b91416a3de10967ad21cc7752574609cd523
Chris Yocum <chryo12@yahoo.com>
2019-07-22 08:45:07 -0400

    Testing fix for cleanup not reading vars

--- root/app/dispatcher.sh
+++ root/app/dispatcher.sh
@@ -14,11 +14,26 @@ DISKOVER_ARRAY[REDIS_PORT]=${REDIS_PORT:-6379}
 
 DISKOVER_ARRAY[DISKOVER_OPTS]="${DISKOVER_ARRAY[DISKOVER_OPTS]} -i ${DISKOVER_ARRAY[INDEX_NAME]}"
 
-declare -p DISKOVER_ARRAY > /config/.diskover_array
+#declare -p DISKOVER_ARRAY > /config/.diskover_array
 
 cd /app/diskover || exit
 
-/bin/bash -c ". /config/.diskover_array; /app/cleanup.sh"
+#/bin/bash -c ". /config/.diskover_array; /app/cleanup.sh"
+# killing existing workers before starting new ones
+echo "killing existing workers..."
+if [ -f "/tmp/diskover_bot_pids" ]; then
+    /bin/bash /app/diskover/diskover-bot-launcher.sh -k > /dev/null 2>&1
+    sleep 3
+fi
+
+# empty existing redis queue
+echo "emptying current redis queues..."
+rq empty -u redis://"${DISKOVER_ARRAY[REDIS_HOST]}":"${DISKOVER_ARRAY[REDIS_PORT]}" diskover_crawl diskover diskover_calcdir failed
+sleep 3
+
+echo "killing dangling workers..."
+/bin/bash /app/diskover/diskover-bot-launcher.sh -k > /dev/null 2>&1
+sleep 3
 
 echo "starting workers with following options: ${DISKOVER_ARRAY[WORKER_OPTS]}"
 /bin/bash /app/diskover/diskover-bot-launcher.sh ${DISKOVER_ARRAY[WORKER_OPTS]}
