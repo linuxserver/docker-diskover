9ad16c8145dd98a558491e50d014d31a79c1a574
Yocum <chyocum@gsiccorp.net>
2019-07-22 18:28:42 -0400

    Cleanup

--- root/app/dispatcher.sh
+++ root/app/dispatcher.sh
@@ -16,27 +16,10 @@ DISKOVER_ARRAY[DISKOVER_OPTS]="${DISKOVER_ARRAY[DISKOVER_OPTS]} -i ${DISKOVER_AR
 
 cd /app/diskover || exit
 
-# Redis Cleanup
 /bin/bash /app/cleanup.sh
-# killing existing workers before starting new ones
-#echo "killing existing workers..."
-#if [ -f "/tmp/diskover_bot_pids" ]; then
-#    /bin/bash /app/diskover/diskover-bot-launcher.sh -k > /dev/null 2>&1
-#    sleep 3
-#fi
-# empty existing redis queue
-#echo "emptying current redis queues..."
-#rq empty -u redis://"${DISKOVER_ARRAY[REDIS_HOST]}":"${DISKOVER_ARRAY[REDIS_PORT]}" diskover_crawl diskover diskover_calcdir failed
-#sleep 3
-# kill dangling workers
-#echo "killing dangling workers..."
-#/bin/bash /app/diskover/diskover-bot-launcher.sh -k > /dev/null 2>&1
-#sleep 3
 
-# Start workers
 echo "starting workers with following options: ${DISKOVER_ARRAY[WORKER_OPTS]}"
 /bin/bash /app/diskover/diskover-bot-launcher.sh ${DISKOVER_ARRAY[WORKER_OPTS]}
 
-# Start crawler
 echo "starting crawler with following options: ${DISKOVER_ARRAY[DISKOVER_OPTS]}"
 /usr/bin/python3 ./diskover.py ${DISKOVER_ARRAY[DISKOVER_OPTS]}
